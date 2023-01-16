930db3a5b8bf723a468bbf281af610524055852d
Stanislas Lange <git@slange.me>
2023-01-16 23:50:24 +0100

    Summarize entry with ChatGPT

--- resources/js/Pages/Feed/Entry.vue
+++ resources/js/Pages/Feed/Entry.vue
@@ -4,11 +4,27 @@ import { Head } from "@inertiajs/vue3";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import utc from "dayjs/plugin/utc";
+import { ref } from "vue";
+import { BrainIcon } from "vue-tabler-icons";
 
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
 
 defineProps(["feed", "entry"]);
+
+const summary = ref("");
+const loading = ref(false);
+
+async function getSummary(entryId) {
+    loading.value = true;
+    const res = await fetch(
+        `http://localhost/api/entry/${entryId}/gpt-summary`
+    );
+    const data = await res.json();
+    console.log(data.summary);
+    summary.value = data.summary;
+    loading.value = false;
+}
 </script>
 
 <template>
@@ -40,11 +56,57 @@ defineProps(["feed", "entry"]);
                         </div>
                     </div>
                 </div>
+                <div class="flex flex-col text-right">
+                    <div class="tooltip" data-tip="Ask ChatGPT for a summary">
+                        <button
+                            class="btn"
+                            :class="{ loading: loading }"
+                            @click="getSummary(entry.id)"
+                        >
+                            <BrainIcon class="mr-2" v-if="!loading" />
+                            Summarize entry
+                        </button>
+                    </div>
+                </div>
             </div>
         </template>
 
         <div class="py-12">
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-3"
+                    v-if="summary"
+                >
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="flex items-center justify-between">
+                            <h3
+                                class="font-semibold text-xl text-gray-800 leading-tight mb-2"
+                            >
+                                Summary
+                            </h3>
+                            <button
+                                className="btn btn-square btn-outline"
+                                @click="summary = ''"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-2 w-2"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                        <div v-html="summary" />
+                    </div>
+                </div>
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <div
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Entry;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/entry/{entry}/gpt-summary', function (Entry $entry) {
+    // https://laravel.com/docs/9.x/cache
+    $summary = Cache::remember("entry-{$entry->id}-gpt-summary", 60 * 60 * 24, function () use ($entry) {
+        $result = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => "Summarize this text: {$entry->content}",
+            // 'temperature' => 0.7,
+            'max_tokens' => 256,
+            // 'top_p' => 1,
+            // 'frequency_penalty' => 0,
+            // 'presence_penalty' => 0,
+        ]);
+
+        return $result->choices[0]->text;
+    });
+
+    return response()->json([
+        'summary' => $summary,
+    ]);
+})->name('entry.gpt-summary');
