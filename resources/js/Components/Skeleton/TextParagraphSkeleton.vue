a45d0cd3c79cd52ba8abb879dd8e4c6e2f3344aa
Stanislas Lange <git@slange.me>
2023-01-17 00:19:59 +0100

    🚸 Add skeleton loader to summary

---
+++ resources/js/Components/Skeleton/TextParagraphSkeleton.vue
@@ -0,0 +1,72 @@
+<!-- https://flowbite.com/docs/components/skeleton/ -->
+<template>
+    <div role="status" class="space-y-2.5 animate-pulse max-w-lg">
+        <div class="flex items-center w-full space-x-2">
+            <div
+                class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+        </div>
+        <div class="flex items-center w-full space-x-2 max-w-[480px]">
+            <div
+                class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+            ></div>
+        </div>
+        <div class="flex items-center w-full space-x-2 max-w-[400px]">
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+        </div>
+        <div class="flex items-center w-full space-x-2 max-w-[480px]">
+            <div
+                class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+            ></div>
+        </div>
+        <div class="flex items-center w-full space-x-2 max-w-[440px]">
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"
+            ></div>
+        </div>
+        <div class="flex items-center w-full space-x-2 max-w-[360px]">
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"
+            ></div>
+            <div
+                class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"
+            ></div>
+        </div>
+        <span class="sr-only">Loading...</span>
+    </div>
+</template>
