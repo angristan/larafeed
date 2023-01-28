02d678d97a1373014edc6bcd817359d1a6e5f129
Stanislas Lange <git@slange.me>
2023-01-28 19:25:36 +0100

    Improve summary close button

--- resources/js/Pages/Feed/Entry.vue
+++ resources/js/Pages/Feed/Entry.vue
@@ -125,27 +125,35 @@ function hideSummary() {
                 >
                     <div class="p-6 bg-white border-b border-gray-200">
                         <div class="flex items-center justify-between">
-                            <h3
-                                class="font-semibold text-xl text-gray-800 leading-tight mb-2"
-                            >
-                                Summary
-                            </h3>
+                            <div class="flex items-center">
+                                <h3
+                                    class="font-semibold text-xl text-gray-800 leading-tight mb-2"
+                                >
+                                    Summary
+                                </h3>
+                                <figcaption class="text-xs text-gray-500 ml-2">
+                                    Powered by ChatGPT
+                                </figcaption>
+                            </div>
                             <button
-                                className="btn btn-square btn-outline"
+                                type="button"
                                 @click="hideSummary"
                                 v-if="summary"
+                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                             >
+                                <span class="sr-only">Close menu</span>
                                 <svg
+                                    class="h-6 w-6"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    className="h-2 w-2"
                                     fill="none"
                                     viewBox="0 0 24 24"
                                     stroke="currentColor"
+                                    aria-hidden="true"
                                 >
                                     <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
                                         d="M6 18L18 6M6 6l12 12"
                                     />
                                 </svg>
