51e581317dd7c587d0879d3d1aa745a7afd5cc9e
Stanislas Lange <git@slange.me>
2024-12-23 17:54:50 +0100

    Update link in Welcome component to point to feeds index

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -45,7 +45,7 @@ export default function Welcome({
                             <nav className="-mx-3 flex flex-1 justify-end">
                                 {auth.user ? (
                                     <Link
-                                        href={route('dashboard')}
+                                        href={route('feeds.index')}
                                         className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Dashboard
