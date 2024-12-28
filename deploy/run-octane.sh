54e4ad05fbf1211ed79c0d1012d12cd8419760b4
Stanislas Lange <git@slange.me>
2024-12-28 15:37:56 +0100

    wip

--- deploy/run-octane.sh
+++ deploy/run-octane.sh
@@ -9,4 +9,4 @@ php artisan view:cache
 
 php artisan migrate --force
 
-php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000
+php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8000 --caddyfile=./Caddyfile
