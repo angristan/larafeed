1f484d0a629d2622cb490c7ff703da6f59fedb51
Stanislas Lange <git@slange.me>
2024-12-28 17:04:18 +0100

    improve

--- deploy/run-scheduler.sh
+++ deploy/run-scheduler.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# This block of code runs the Laravel scheduler every minute
 while true; do
     echo "Running the scheduler..."
     php artisan schedule:run --verbose --no-interaction &
--- deploy/run-web.sh
+++ deploy/run-web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run migrations, process the Nginx configuration template and start Nginx
-php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (
-    php-fpm -y /assets/php-fpm.conf &
-    nginx -c /nginx.conf
-)
+php artisan optimize
+
+php artisan migrate --force
+
+php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8080 --log-level=info --caddyfile=./deploy/Caddyfile
