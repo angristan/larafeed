d554fafe4cb28bcd2c79adc2fdeef54917d306d1
Stanislas Lange <git@slange.me>
2024-12-29 20:45:49 +0100

    Reader: lazily evaluate feeds and entries

--- app/Actions/ShowFeedReader.php
+++ app/Actions/ShowFeedReader.php
@@ -21,73 +21,77 @@ class ShowFeedReader
         $entry_id = $request->query('entry');
         $filter = $request->query('filter');
 
-        $feeds = Auth::user()
-            ->feeds()
-            ->withCount('entries')
-            ->orderBy('name')
-            ->get()->map(fn (Feed $feed) => [
-                'id' => $feed->id,
-                'name' => $feed->subscription?->custom_feed_name ?? $feed->name,
-                'site_url' => $feed->site_url,
-                'favicon_url' => $feed->favicon_url(),
-                'entries_count' => $feed->entries_count,
-                'last_successful_refresh_at' => $feed->last_successful_refresh_at,
-                'last_failed_refresh_at' => $feed->last_failed_refresh_at,
-            ]);
+        $getFeedsFn = function () {
+            return Auth::user()
+                ->feeds()
+                ->withCount('entries')
+                ->orderBy('name')
+                ->get()->map(fn (Feed $feed) => [
+                    'id' => $feed->id,
+                    'name' => $feed->subscription?->custom_feed_name ?? $feed->name,
+                    'site_url' => $feed->site_url,
+                    'favicon_url' => $feed->favicon_url(),
+                    'entries_count' => $feed->entries_count,
+                    'last_successful_refresh_at' => $feed->last_successful_refresh_at,
+                    'last_failed_refresh_at' => $feed->last_failed_refresh_at,
+                ]);
+        };
 
-        $entries = Entry::query()
-            // Apply optional filters
-            ->when($feed_id, fn ($query) => $query->where('entries.feed_id', $feed_id))
-            ->when($filter === 'unread', fn ($query) => $query->whereNull('entry_interactions.read_at'))
-            ->when($filter === 'read', fn ($query) => $query->whereNotNull('entry_interactions.read_at'))
-            ->when($filter === 'favorites', fn ($query) => $query->whereNotNull('entry_interactions.starred_at'))
-            // Only show entries from feeds the user is subscribed to
-            ->join('feed_subscriptions', function ($join) {
-                $join->on('entries.feed_id', '=', 'feed_subscriptions.feed_id')
-                    ->where('feed_subscriptions.user_id', '=', Auth::id());
-            })
-            // Fetch the user interaction for each entry
-            ->leftJoin('entry_interactions', function ($join) {
-                $join->on('entries.id', '=', 'entry_interactions.entry_id')
-                    ->where('entry_interactions.user_id', '=', Auth::id());
-            })
-            // Fetch the feed for each entry
-            ->join('feeds', 'entries.feed_id', '=', 'feeds.id')
-            ->select([
-                'entries.id',
-                'entries.title',
-                'entries.url',
-                'entries.author',
-                'entries.content',
-                'entries.published_at',
-                'entry_interactions.read_at',
-                'entry_interactions.starred_at',
-                'entry_interactions.archived_at',
-                'feeds.id as feed_id',
-                'feeds.name as feed_name',
-                'feeds.favicon_url as feed_favicon_url',
+        $getEntriesFn = function () use ($feed_id, $filter) {
+            return Entry::query()
+               // Apply optional filters
+                ->when($feed_id, fn ($query) => $query->where('entries.feed_id', $feed_id))
+                ->when($filter === 'unread', fn ($query) => $query->whereNull('entry_interactions.read_at'))
+                ->when($filter === 'read', fn ($query) => $query->whereNotNull('entry_interactions.read_at'))
+                ->when($filter === 'favorites', fn ($query) => $query->whereNotNull('entry_interactions.starred_at'))
+               // Only show entries from feeds the user is subscribed to
+                ->join('feed_subscriptions', function ($join) {
+                    $join->on('entries.feed_id', '=', 'feed_subscriptions.feed_id')
+                        ->where('feed_subscriptions.user_id', '=', Auth::id());
+                })
+               // Fetch the user interaction for each entry
+                ->leftJoin('entry_interactions', function ($join) {
+                    $join->on('entries.id', '=', 'entry_interactions.entry_id')
+                        ->where('entry_interactions.user_id', '=', Auth::id());
+                })
+               // Fetch the feed for each entry
+                ->join('feeds', 'entries.feed_id', '=', 'feeds.id')
+                ->select([
+                    'entries.id',
+                    'entries.title',
+                    'entries.url',
+                    'entries.author',
+                    'entries.content',
+                    'entries.published_at',
+                    'entry_interactions.read_at',
+                    'entry_interactions.starred_at',
+                    'entry_interactions.archived_at',
+                    'feeds.id as feed_id',
+                    'feeds.name as feed_name',
+                    'feeds.favicon_url as feed_favicon_url',
 
-            ])
-            // Fetch the feed for each entry
-            ->orderByDesc('entries.published_at')
-            ->limit(100)
-            ->get()
-            ->map(fn ($entry) => [
-                'id' => $entry->id,
-                'title' => $entry->title,
-                'url' => $entry->url,
-                'author' => $entry->author,
-                'content' => $entry->content,
-                'published_at' => $entry->published_at,
-                'read_at' => $entry->read_at,
-                'starred_at' => $entry->starred_at,
-                'archived_at' => $entry->archived_at,
-                'feed' => [
-                    'id' => $entry->feed_id,
-                    'name' => $entry->feed_name,
-                    'favicon_url' => BuildProfixedFaviconURL::run($entry->feed_favicon_url),
-                ],
-            ]);
+                ])
+               // Fetch the feed for each entry
+                ->orderByDesc('entries.published_at')
+                ->limit(100)
+                ->get()
+                ->map(fn ($entry) => [
+                    'id' => $entry->id,
+                    'title' => $entry->title,
+                    'url' => $entry->url,
+                    'author' => $entry->author,
+                    'content' => $entry->content,
+                    'published_at' => $entry->published_at,
+                    'read_at' => $entry->read_at,
+                    'starred_at' => $entry->starred_at,
+                    'archived_at' => $entry->archived_at,
+                    'feed' => [
+                        'id' => $entry->feed_id,
+                        'name' => $entry->feed_name,
+                        'favicon_url' => BuildProfixedFaviconURL::run($entry->feed_favicon_url),
+                    ],
+                ]);
+        };
 
         $getCurrentEntryFn = function () use ($request, $entry_id): Entry|null {
             if (! $entry_id) {
@@ -181,9 +185,8 @@ class ShowFeedReader
 
         // TODO https://laravel.com/docs/9.x/eloquent-resources
         return Inertia::render('Reader/Reader', [
-            // TODO lazy
-            'feeds' => $feeds,
-            'entries' => $entries,
+            'feeds' => $getFeedsFn,
+            'entries' => $getEntriesFn,
             'currententry' => $getCurrentEntryFn,
             'unreadEntriesCount' => $unreadEntriesCountFn,
             'readEntriesCount' => $readEntriesCountFn,
