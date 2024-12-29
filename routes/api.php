6aa422fa173ecd749b881b9dcb7221b53f19a6fe
Stanislas Lange <git@slange.me>
2024-12-29 20:40:16 +0100

    Remove unused GPT summary route from API

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Actions\Entry\SummarizeEntryWithLLM;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/entry/{entry}/gpt-summary', SummarizeEntryWithLLM::class)->name('entry.gpt-summary');
-});
