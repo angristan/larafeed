908fc1929c56726687726deecb726c4e74bd694e
Stanislas Lange <git@slange.me>
2024-12-30 18:38:26 +0100

    Add max-requests option to octane/frankenphp

--- deploy/run-web.sh
+++ deploy/run-web.sh
@@ -4,4 +4,9 @@ php artisan optimize
 
 php artisan migrate --force
 
-php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=8080 --log-level=info --caddyfile=./deploy/Caddyfile
+php artisan octane:start --server=frankenphp \
+    --host=0.0.0.0 \
+    --port=8080 \
+    --log-level=info \
+    --caddyfile=./deploy/Caddyfile \
+    --max-requests=100
