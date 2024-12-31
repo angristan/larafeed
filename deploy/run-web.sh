a3916a231e4ba0989f1d2227fff442b2f77aeba1
Stanislas Lange <git@slange.me>
2024-12-31 18:30:11 +0100

    Run frankenphp without octane

--- deploy/run-web.sh
+++ deploy/run-web.sh
@@ -4,9 +4,4 @@ php artisan optimize
 
 php artisan migrate --force
 
-php artisan octane:start --server=frankenphp \
-    --host=0.0.0.0 \
-    --port=8080 \
-    --log-level=info \
-    --caddyfile=./deploy/Caddyfile \
-    --max-requests=100
+frankenphp run --config /etc/caddy/Caddyfile --adapter caddyfile
