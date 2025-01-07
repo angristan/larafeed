cfb224dcde2a08e4ac30fa1baf1c04699ce34e26
Stanislas Lange <git@slange.me>
2025-01-07 16:01:44 +0100

    Add reminder for Redis client timeout in deployment script

--- deploy/run-web.sh
+++ deploy/run-web.sh
@@ -4,6 +4,10 @@ php artisan optimize
 
 php artisan migrate --force
 
+# Remember to set a redis client timeout so that the container can go to sleep on Railway
+# As with Octane, connections stay open for max-requests
+# CONFIG SET timeout 10
+
 php artisan octane:start --server=frankenphp \
     --host=0.0.0.0 \
     --port=8080 \
