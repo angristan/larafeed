917d46ccd3c0122ef40fea4be61a3c5442593f3d
Stanislas Lange <git@slange.me>
2025-01-09 17:19:25 +0100

    Refactor API routes to use fully qualified class names for better clarity

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Actions\FeverAPI\HandleRequest;
-use App\Actions\GoogleReaderAPI\ClientLogin;
-use App\Actions\GoogleReaderAPI\EditTag;
-use App\Actions\GoogleReaderAPI\GetStreamContents;
-use App\Actions\GoogleReaderAPI\GetStreamItemIds;
-use App\Actions\GoogleReaderAPI\GetSubscriptionList;
-use App\Actions\GoogleReaderAPI\GetToken;
-use App\Actions\GoogleReaderAPI\GetUserInfo;
+use App\Actions\FeverAPI;
+use App\Actions\GoogleReaderAPI;
 use App\Http\Middleware\CheckFeverApiToken;
 use App\Http\Middleware\CheckGoogleReaderToken;
 use Illuminate\Http\Request;
@@ -21,17 +15,17 @@ Route::get('/user', function (Request $request) {
 
 // Google Reader API
 Route::prefix('/reader')->group(function () {
-    Route::post('accounts/ClientLogin', ClientLogin::class);
+    Route::post('accounts/ClientLogin', GoogleReaderAPI\ClientLogin::class);
 
     Route::prefix('/reader/api/0')
         ->middleware(CheckGoogleReaderToken::class)
         ->group(function () {
-            Route::get('user-info', GetUserInfo::class);
-            Route::get('token', GetToken::class);
-            Route::get('subscription/list', GetSubscriptionList::class);
-            Route::get('stream/items/ids', GetStreamItemIds::class);
-            Route::post('stream/items/contents', GetStreamContents::class);
-            Route::post('edit-tag', EditTag::class);
+            Route::get('user-info', GoogleReaderAPI\GetUserInfo::class);
+            Route::get('token', GoogleReaderAPI\GetToken::class);
+            Route::get('subscription/list', GoogleReaderAPI\GetSubscriptionList::class);
+            Route::get('stream/items/ids', GoogleReaderAPI\GetStreamItemIds::class);
+            Route::post('stream/items/contents', GoogleReaderAPI\GetStreamContents::class);
+            Route::post('edit-tag', GoogleReaderAPI\EditTag::class);
         });
 });
 
@@ -39,5 +33,5 @@ Route::prefix('/reader')->group(function () {
 Route::prefix('/fever')
     ->middleware(CheckFeverApiToken::class)
     ->group(function () {
-        Route::match(['get', 'post'], '/', HandleRequest::class);
+        Route::match(['get', 'post'], '/', FeverAPI\HandleRequest::class);
     });
