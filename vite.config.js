8c3c9c1a1351e5014dc5eb1d0252859170df6228
Stanislas Lange <git@slange.me>
2025-01-27 21:26:07 +0100

    Enable react compiler

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,25 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
+const ReactCompilerConfig = {
+    target: '18', // '17' | '18' | '19'
+};
+
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
             refresh: true,
         }),
-        react(),
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+            },
+        }),
     ],
     resolve: {
         alias: {
-            // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+            // /esm/icons/index.mjs only exports the icons statically, so no scoeparate chunks are created
             '@tabler/icons-react':
                 '@tabler/icons-react/dist/esm/icons/index.mjs',
         },
