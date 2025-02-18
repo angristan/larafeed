511fffbfda2d8b38aef71106d7ee748e12c729ae
Stanislas Lange <git@slange.me>
2025-02-18 19:53:21 +0100

    Optimize feed refresh prioritization based on last entry date and last refresh time

--- app/Actions/Feed/RefreshFeeds.php
+++ app/Actions/Feed/RefreshFeeds.php
@@ -25,17 +25,52 @@ class RefreshFeeds
     }
 
     /**
-     * Refresh all the feeds asynchronously
-     * by queuing a job for each feed
+     * Refresh all the feeds asynchronously by queuing a job for each feed
+     * Priority is given to to combination of:
+     *      feeds that haven't been refreshed in a while
+     *      feeds where the last entry is recent
      */
     public function asJob(): void
     {
-
-        Feed::orderByRaw('LEAST(last_successful_refresh_at, last_failed_refresh_at) ASC')
-            ->where('last_successful_refresh_at', '<', now()->subMinutes(60))
-            ->limit(20)->get()->each(
-                fn (Feed $feed) => RefreshFeedEntries::dispatch($feed)
-            );
+        Feed::query()
+            ->leftJoin('entries', 'feeds.id', '=', 'entries.feed_id')
+            // Select the feed and the last entry published_at date
+            ->select('feeds.*', \DB::raw('MAX(entries.published_at) as last_entry_at'))
+            ->groupBy('feeds.id')
+            // Select feeds that haven't been refreshed in the last hour
+            ->whereRaw('LEAST(last_successful_refresh_at, last_failed_refresh_at) < ?', [now()->subMinutes(60)])
+            // Order by the ratio of time since last refresh and time since last entry
+            ->orderByRaw(<<<'SQL'
+                CASE
+                    -- We fetch the entries when we add the feed
+                    -- So if there are no entries, the feed has never been working
+                    WHEN MAX(entries.published_at) IS NULL THEN (
+                        -- ratio of time since last refresh and 10 years ago
+                        -- so this should give a lower ratio to feeds that have never been working
+                        -- because we treat them as if their last entry was 10 years ago
+                        -- we still want to refresh them from time to time
+                        (
+                            EXTRACT(EPOCH FROM NOW()) - EXTRACT(EPOCH FROM LEAST(last_successful_refresh_at, last_failed_refresh_at))
+                        ) /
+                        (
+                            EXTRACT(EPOCH FROM NOW()) - (EXTRACT(EPOCH FROM NOW() - INTERVAL '10 year'))
+                        )
+                    )
+                    -- ratio of time since last refresh and time since last entry
+                    -- we want to refresh feeds that have not been refreshed in a while
+                    -- but we want to prioritize feeds that have recent entries
+                    ELSE (
+                        EXTRACT(EPOCH FROM NOW()) - EXTRACT(EPOCH FROM LEAST(last_successful_refresh_at, last_failed_refresh_at))
+                    ) / NULLIF( -- Avoid division by zero in the very unlikely case where now = the most recent entry
+                        EXTRACT(EPOCH FROM NOW()) - EXTRACT(EPOCH FROM MAX(entries.published_at)),
+                        0
+                    )
+                END DESC
+                SQL
+            )
+            ->limit(10)
+            ->get()
+            ->each(fn (Feed $feed) => RefreshFeedEntries::dispatch($feed));
     }
 
     public function asCommand(Command $command): void
