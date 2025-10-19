6c387ac5ded978b5dcbcacea4b01536ba9bd9085
Stanislas Lange <git@slange.me>
2025-10-19 18:24:32 +0200

    test

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Verified;
 
 class VerifyEmailController extends Controller
 {
@@ -21,7 +22,7 @@ class VerifyEmailController extends Controller
         }
 
         if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+            Event::dispatch(new Verified($request->user()));
         }
 
         return redirect()->intended(route('feeds.index', absolute: false).'?verified=1');
